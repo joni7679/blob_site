9a7c73afb47ee8b367b54ac5754c23816f9354bb
User 1 <you@example.com>
2023-08-18 16:58:27 +0530

    update

--- js/script.js
+++ js/script.js
@@ -97,3 +97,62 @@ $('.gallery-1').slick({
 });
 
 
+$('.pricing-silder').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: false,
+                autoplay: true,
+            }
+        },
+
+        {
+            breakpoint: 950,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 580,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+$(".prev-btn").click(function () {
+    $(".pricing-silder").slick("slickPrev");
+});
+
+$(".next-btn").click(function () {
+    $(".pricing-silder").slick("slickNext");
+});
+
+
+
+
+
